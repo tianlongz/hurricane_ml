710d72a06da6fd52f5633be1e436c8ae5fb5db2f
Tianlong Zhang <tianlong.zhang@utah.edu>
2019-12-16 12:34:12 -0700

    read data

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pandas as pd
 import scipy as sci
 import numpy as np
+import os
+import csv
 
-path = ""
+current_path = os.getcwd()
+input_path = current_path + '/data/Input/'
+output_path = current_path + '/data/ML/Results/'
 
-input = np.zeros()
+
+for i in range(1):
+    input = pd.read_csv(input_path + 'Input' + str(i + 1) + ".txt", delimiter='\t', header=None).values
+
+    output = pd.read_csv(output_path + 'Commitment' + str(i + 1) + '.txt', delimiter=' ', header=None).values[0]
+    output = output[0:-1]
+    output = np.reshape(output, (24, 54))
+    print(output.shape)
+    print(output[0])
+
+
